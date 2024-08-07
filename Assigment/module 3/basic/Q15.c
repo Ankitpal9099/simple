c5e4fa0cf9d6851c995d41d6a026b09ee5dc35c6
Ankitpal9099 <ankitpal9969@gmail.com>
2024-08-07 11:44:25 +0530

    add folder

--- Assigment/module 3/basic/Q15.c
+++ Assigment/module 3/basic/Q15.c
@@ -1,1 +1,18 @@
 //.Convert school’s name in abbreviated form
+#include <stdio.h>
+
+int main() {
+    char schoolName[100];
+    printf("Enter the school's name: ");
+    scanf("%c[^\n]", schoolName);
+
+    printf("Abbreviation: ");
+    for (int i = 0; schoolName[i] != '\0'; i++) {
+        if (i == 0 || schoolName[i - 1] == ' ') {
+            printf("%c", schoolName[i]);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
--- Assigment/module 3/basic/Q16.c
+++ Assigment/module 3/basic/Q16.c
@@ -1,1 +1,18 @@
 //.Convert country’s name in abbreviate form
+#include <stdio.h>
+
+int main() {
+    char countryName[100];
+    printf("Enter the country's name: ");
+    scanf("%s[^\n]", countryName);
+
+    printf("Abbreviation: ");
+    for (int i = 0; countryName[i] != '\0'; i++) {
+        if (i == 0 || countryName[i - 1] == ' ') {
+            printf("%c",countryName[i]);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
